fe18b8b11e1c763113bb03495aea0fa097698b91
MrZWH <1875290692@qq.com>
2017-08-22 16:29:24 +0800

    initial

---
+++ src/page/index/index.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: whz 
+ * @Date: 2017-08-22 10:12:02 
+ * @Last Modified by: whz
+ * @Last Modified time: 2017-08-22 15:19:08
+ */
+require('./index.css');
---
+++ src/page/login/index.js
@@ -0,0 +1,6 @@
+/*
+ * @Author: whz 
+ * @Date: 2017-08-22 10:11:39 
+ * @Last Modified by:   whz 
+ * @Last Modified time: 2017-08-22 10:11:39 
+ */
