55b28a515a1a5dbb5d4cb10dd2983456bd1460fc
MrZWH <1875290692@qq.com>
2017-09-01 16:55:00 +0800

    payment

--- src/page/result/index.js
+++ src/page/result/index.js
@@ -2,7 +2,7 @@
  * @Author: whz 
  * @Date: 2017-08-24 10:20:43 
  * @Last Modified by: whz
- * @Last Modified time: 2017-08-24 10:33:57
+ * @Last Modified time: 2017-09-01 16:53:20
  */
 
 'use strict';
@@ -14,7 +14,11 @@ var _mm = require('util/mm.js');
 $(function() {
     var type = _mm.getUrlParam('type') || 'default',
         $element = $('.' + type + '-success');
-
+    if(type === 'payment') {
+        var orderNumber =_mm.getUrlParam('orderNumber'),
+            $orderNumber = $element.find('.order-number');
+        $orderNumber.attr('href', $orderNumber.attr('href') + orderNumber)
+    }
     // 显示对应的提示元素
     $element.show();
 })
